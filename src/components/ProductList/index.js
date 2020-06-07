944771e8efaef1099e6e74131f3c4b9cfd0239df
Matias Rivas <matias.rivas@appdirect.com>
2020-06-07 19:12:29 -0300

    Created productList table

--- src/components/ProductList/index.js
+++ src/components/ProductList/index.js
@@ -1,36 +1,79 @@
 import React, { useContext, useState, useEffect } from "react";
-import { Card, Button } from "semantic-ui-react";
+import { Table } from "semantic-ui-react";
 import { Context } from "../../Context";
 import "./styles.scss";
+
+const columns = [
+  { key: "codigo", label: "Codigo" },
+  { key: "nombre", label: "Nombre" },
+  { key: "precio", label: "Precio" },
+  { key: "precioCosto", label: "Costo" },
+  { key: "iva", label: "iva" },
+  { key: "descripcion", label: "Descripcion" }
+];
 
 const ProductList = ({ productList = [] }) => {
   const { token } = useContext(Context);
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
 
   useEffect(
     function () {
       if (productList) {
-        setProducts(productList);
+        const parsedProducts = productList.map((prod) => {
+          return {
+            codigo: prod.codigo,
+            descripcion: prod.descripcion,
+            id: prod.id,
+            nombre: prod.nombre,
+            precio: prod.precio,
+            precioCosto: prod.precioCosto,
+            userid: prod.userid,
+            iva: prod.iva.texto,
+          };
+        });
+        setProducts(parsedProducts);
+        setFilteredProducts(parsedProducts);
       }
     },
     [productList]
   );
 
-  const renderProducts = () => (
-    <Card.Group className="productListContainer">
-      {products.map((product, index) => {
-        return (
-          <Card inverted="true" key={`prod-${index}`}>
-            <Card.Content>
-            Product!
-            </Card.Content>
-          </Card>
-        );
-      })}
-    </Card.Group>
-  );
+  const renderProducts = () => {
+    return (
+      <Table size="small" celled selectable>
+        <Table.Header>
+          <Table.Row>
+            {columns.map((column, index) => {
+              return (
+                <Table.HeaderCell key={`main-column-${column}-${index}`}>
+                  {column.label}
+                </Table.HeaderCell>
+              );
+            })}
+          </Table.Row>
+        </Table.Header>
 
-  return <div>{products.length && renderProducts()}</div>;
+        <Table.Body>
+          {filteredProducts.map((product) => {
+            return (
+              <Table.Row key={`product${product.id}row`}>
+                {columns.map((column) => {
+                  return (
+                    <Table.Cell key={`${product.id}[${column.label}]`}>
+                      {product[column.key]}
+                    </Table.Cell>
+                  );
+                })}
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    );
+  };
+
+  return <div>{filteredProducts.length && renderProducts()}</div>;
 };
 
 export default ProductList;
