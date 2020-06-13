1e56faca3b6fc50b00e2be0dee115585df35e5c3
Matias Rivas <matias.rivas@appdirect.com>
2020-06-13 12:37:23 -0300

    Added debounce function

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,14 @@ export const searchInArr = (arr, searchKey) => {
     })
   );
 };
+
+export const debounce = (fn, delay) => {
+  let timer = null;
+  return function (...args) {
+    const context = this;
+    timer && clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(context, args);
+    }, delay);
+  };
+};
